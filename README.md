60032d3fcb3bc828318cd962b22a81981cbd672d
CharlesRWilson <charlesrwilson10@gmail.com>
2024-08-21 22:46:13 -0600

    updated my portfolio links some more!

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# react-portfolio-cw
+# react-portfolio-cw#
+
+## Description
+
+I loved making this portfolio! I got lots of practice with React when I made it and it helped me to make my work feel more professional!
+
+## Installation
+
+Open the [link!](https://crwportfolio.netlify.app)
+
+
+## Credits
+
+Heres my GitHub! [CharlesRWilson](https://github.com/CharlesRWilson)
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -13,8 +13,32 @@ const Portfolio = () => {
     {
       title: 'Movies N Muchies!',
       image: '/path-to-project2-image.jpg',
-      deployedLink: ' https://tishthegoat.github.io/Movies-n-Munchies/',
+      deployedLink: 'https://tishthegoat.github.io/Movies-n-Munchies/',
       repoLink: ' https://github.com/tishthegoat/Movies-n-Munchies.git',
+    },    
+    {
+      title: 'Employee Tracker',
+      image: '/path-to-project2-image.jpg',
+      deployedLink: 'https://charlesrwilson.github.io/employee-js-tracker-cw/',
+      repoLink: 'https://github.com/CharlesRWilson/employee-js-tracker-cw.git',
+    },
+    {
+      title: 'Personal Blog',
+      image: '/path-to-project1-image.jpg',
+      deployedLink: 'https://charlesrwilson.github.io/challenge4repo/',
+      repoLink: 'https://github.com/CharlesRWilson/challenge4repo.git',
+    },
+    {
+      title: 'Weather Dashboard',
+      image: '/path-to-project2-image.jpg',
+      deployedLink: 'https://charlesrwilson.github.io/weather-dashboard-cw/',
+      repoLink: 'https://github.com/CharlesRWilson/weather-dashboard-cw.git',
+    },    
+    {
+      title: 'ReadMe Generator',
+      image: '/path-to-project2-image.jpg',
+      deployedLink: 'https://drive.google.com/file/d/1mRh5CB-SmJBwsjqVofmITh6JwJTZmS8V/view?usp=drive_link',
+      repoLink: 'https://github.com/CharlesRWilson/readme-generator-cw',
     },
   ];
 
